f65b24b30dd66aa9aaf7215a141b88b12f5d370b
AndreVictor60 <andre.victor60@free.fr>
2017-09-29 15:03:26 +0200

    Ecriture de l'exercice 5

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+function mafonction($var1,$var2){
+  echo "Votre nom est  ".$var1. " et votre prénom est ".$var2."<br />";
+  $fusion = $var1.$var2;
+  echo "La fusion est ".$fusion;
+
+}
+mafonction("Andre","Victor");
+ ?>
