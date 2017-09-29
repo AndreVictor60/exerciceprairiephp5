4a6942e91593039ab16b041e19702c6da558997d
AndreVictor60 <andre.victor60@free.fr>
2017-09-29 16:19:43 +0200

    Ecriture de l'exercice 5

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
+//  fonction qui prend en paramètre deux chaînes de caractère et qui retourne la fusion de ces deux chaînes.
 function mafonction($var1,$var2){
   echo "Votre nom est  ".$var1. " et votre prénom est ".$var2."<br />";
+  // Fusionner les chaînes de caractère des paramètre
   $fusion = $var1.$var2;
   echo "La fusion est ".$fusion;
 
 }
+//Appel de la fonction
 mafonction("Andre","Victor");
  ?>
